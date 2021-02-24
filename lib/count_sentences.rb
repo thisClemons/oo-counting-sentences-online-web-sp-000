dec329a89d6a532895a3ae13fdbf8f26ab36aef2
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-24 19:05:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1,1] == "."
   end
 
   def question?
+    self[-1,1] == "?"
 
   end
 
   def exclamation?
+    self[-1,1] == "!"
 
   end
 
   def count_sentences
+    chars = self.split(/[.!?]+/)
 
+    #binding.pry
+
+    chars.count
+
+
+    # punc = [".", "!", "?"]
+    # sentences = 0
+    #
+    # chars.each_with_index do |char, i|
+    #   sentences += 1 if punc.include?(char) && !punc.include?(chars[i-1])
+    #   binding.pry
+    # end
+    # sentences
   end
 end
+
+"Hi! This is a test... Good?".count_sentences
